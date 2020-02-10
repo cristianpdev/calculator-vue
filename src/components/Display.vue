27be38f8c59fb9955426ccdd5865bc6a8bb875a9
Cristian Penteado <cristianpenteado@protonmail.ch>
2020-02-09 21:40:03 -0300

    calculator with basics operations

---
+++ src/components/Display.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="display">
+      {{ value }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value']
+}
+</script>
+
+<style>
+.display {
+  grid-column: span 4;
+  background-color: #0004;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 2.1em;
+  padding: 20px;
+  overflow: hidden;
+}
+</style>
